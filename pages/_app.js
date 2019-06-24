13dfd84d7ebc3d520b08341a1fb74f3e4025ed54
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2019-06-24 23:26:31 +0700

    Show loading and prefetch in home

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,23 @@ import App, { Container } from 'next/app';
 import Layout from '../components/Layout';
 
 import Router from 'next/router';
+import NProgress from 'nprogress'
 
+Router.events.on('routeChangeStart', url => {
+  console.log(`Loading: ${url}`)
+  NProgress.start()
+})
 Router.events.on('routeChangeComplete', () => {
   if (process.env.NODE_ENV !== 'production') {
     const els = document.querySelectorAll('link[href*="/_next/static/css/styles.chunk.css"]');
     const timestamp = new Date().valueOf();
     els[0].href = '/_next/static/css/styles.chunk.css?v=' + timestamp;
   }
+
+  NProgress.done()
 })
+Router.events.on('routeChangeError', () => NProgress.done())
+
 class MyApp extends App {
 
   static async getInitialProps({ Component, ctx }) {
