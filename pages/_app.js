d322d38a8d4632990143de4f30dcb86c2326b05d
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2019-06-24 19:23:57 +0700

    Try fix delay load antd

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,15 @@ import React from 'react';
 import App, { Container } from 'next/app';
 import Layout from '../components/Layout';
 
+import Router from 'next/router';
+
+Router.events.on('routeChangeComplete', () => {
+  if (process.env.NODE_ENV !== 'production') {
+    const els = document.querySelectorAll('link[href*="/_next/static/css/styles.chunk.css"]');
+    const timestamp = new Date().valueOf();
+    els[0].href = '/_next/static/css/styles.chunk.css?v=' + timestamp;
+  }
+})
 class MyApp extends App {
 
   static async getInitialProps({ Component, ctx }) {
