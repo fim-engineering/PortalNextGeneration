a20a38373d4a865eeaf2ad2528044eae789dc994
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2019-06-27 20:22:24 +0700

    Adding axios and fetch data to server login

--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,48 @@
 import { GoogleLogin } from 'react-google-login';
-const { GOOGLE_CLIENT_ID } = process.env
+import axios from 'axios';
 
 class Login extends React.Component {
 
-  responseGoogle = (response) => {
+  onSuccessLogin = async (res) => {
+    
+    const { profileObj, googleId, imageUrl, givenName, familyName } = res;
+
+    try {
+      
+      const data = await axios.post(`${process.env.URL_BACKEND}/auth/login`, {
+        email: profileObj.email,
+        socialId: googleId,
+        loginSource: "Google",
+        profilPicture: imageUrl,
+        firstName: givenName,
+        lastName: familyName
+      })
+  
+      console.log("data: ", data)
+      
+    } catch (error) {
+      console.log("error: ", error)
+    }
+  }
+
+  responseGoogle = async (response) => {
     console.log(response);
+
+
+    if (response.accessToken) {
+      this.onSuccessLogin(response)
+    } else {
+      console.log("GAGAL GOOGLE")
+    }
+    
   }
 
   render() {
-    console.log("process.env.API_HOST: ", GOOGLE_CLIENT_ID)
+    console.log("process.env.API_HOST: ", process.env.GOOGLE_CLIENT_ID)
+    console.log("process.env.URL_BACKEND: ", process.env.URL_BACKEND)
     return <React.Fragment>
       <GoogleLogin
-        clientId={GOOGLE_CLIENT_ID}
+        clientId={process.env.GOOGLE_CLIENT_ID}
         buttonText="Login"
         onSuccess={this.responseGoogle}
         onFailure={this.responseGoogle}
