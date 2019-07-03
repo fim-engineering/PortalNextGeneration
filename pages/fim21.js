d3bff0012e5879866de88c65bbf577bc7d8fb458
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2019-07-03 22:21:49 +0700

    remove unused code

--- pages/fim21.js
+++ pages/fim21.js
@@ -4,32 +4,16 @@ import {auth} from '@HoC/withAuth';
 import {withUser} from '@HoC/withUser';
 import Container from '@components/FIM21'
 
-// function fim21(props) {
-//   return <Container {...props} />
-// }
-
-class fim21 extends Component {
-  static getInitialProps = async (ctx) => {
-    const { token_FIM, step } = await auth(ctx);
-    const { dataUser } = await withUser(ctx)
-
-    return { token_FIM, step, dataUser }
-  }
-
-  render() {
-    console.log("this.props fim21: ", this.props)
-
-    return <Container {...this.props} />
-  }
+function fim21(props) {
+  return <Container {...props} />
 }
 
-// fim21.getInitialProps = async ctx => {
-//   // Check user's session
-//   const { token_FIM, step } = auth(ctx);
-//   const { dataUser } = withUser(ctx)
+fim21.getInitialProps = async ctx => {
+  // Check user's session
+  const { token_FIM, step } = await auth(ctx);
+  const { dataUser } = await withUser(ctx)
 
-//   return { token_FIM, step, dataUser }
-// }
+  return { token_FIM, step, dataUser }
+}
 
-// export default requireLogin(fim21);
 export default fim21;
